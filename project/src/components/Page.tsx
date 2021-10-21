9c0b6819e2973a113e20889b2a617531fa3db3b6
pabloapausa <pabloapausa@outlook.com>
2021-10-21 14:19:05 +0200

    Fix routes

--- project/src/components/Page.tsx
+++ project/src/components/Page.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-import Input from '../input/input';
-import Placeholder from '../placeholder/placeholder';
+import Input from './input/Input';
+import Placeholder from './placeholder/Placeholder';
 import List from './list/List';
 
 export default function Page() {
